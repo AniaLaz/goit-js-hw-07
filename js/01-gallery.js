309dabfe6e87f5d7bdbab1af0bf7519d99631dce
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-09-19 22:42:34 +0300

    1-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryCards = createGalaryCards(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryCards)
+
+function createGalaryCards(galleryItems) {
+    return galleryItems.map(({description,original,preview}) =>{
+        return `   
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    }).join('');   
+}
 
 console.log(galleryItems);
+console.log(galleryEl);
